4be0c66106e3bf008a1bbef34399b052f8cae1f4
Fawaz Siddiqi <mfawazsiddiqi@gmail.com>
2018-12-03 18:39:03 +0400

    Initial Commit

---
+++ MohammadFawazSiddiqi_5465163_CSCI251Ass3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MohammadFawazSiddiqi_5465163_CSCI251Ass3
+//
+//  Created by Fawaz Siddiqi on 12/3/18.
+//  Copyright © 2018 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
